f9b1b943ab751523014d701ad40142148e5229e0
Yahjairav23 <vyahjaira23@gmail.com>
2021-06-03 21:48:55 -0400

    intro and projects

--- portfolio/src/components/Intro.js
+++ portfolio/src/components/Intro.js
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function Intro() {
     return(
-        <header>This is the Intro</header>
+        <div class="intro-div">
+            <h1 class="introduction"> Hi! My name's Yahjaira (yah-hi-rah). </h1>
+            <h2 class="tagline">{"<"} I'm coding for the future. {">"}</h2>
+        </div>
     )
 };
 
--- portfolio/src/components/ProjectsContainer.js
+++ portfolio/src/components/ProjectsContainer.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 export default function  ProjectsContainer() {
     return(
+        <>
         <h2>These are my projects</h2>
+        <ul>
+            <li><a href="#">Project 1</a></li>
+            <li><a href="#">Project 2</a></li>
+            <li><a href="#">Project 3</a></li>
+        </ul>
+        </>
     )
 };
 
